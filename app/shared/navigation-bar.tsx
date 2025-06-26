7fc3a86661676f1ba59d9f18c13315b6fe5c86e4
ge28dib <ge28dib@mytum.de>
2025-06-26 22:47:09 +0200

    diggaa wtf

--- app/shared/navigation-bar.tsx
+++ app/shared/navigation-bar.tsx
@@ -1,70 +1,63 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link, useLocation } from "react-router";
 import "./navigation-bar.scss";
-import { Link } from "react-router";
-import { useState } from "react";
-import HomeIcon from "./navbar-icons/home-icon";
-import AccountIcon from "./navbar-icons/account-icon";
-import TrackIcon from "./navbar-icons/track-icon";
+
+// Icon-Imports (angepasster Pfad)
+import HomeFilled from "../../resources/icons/house-2-fill.svg";
+import HomeOutline from "../../resources/icons/house-2.svg";
+import TrackFilled from "../../resources/icons/pen-writing-filled.svg";
+import TrackOutline from "../../resources/icons/pen-writing.svg";
+import AccountFilled from "../../resources/icons/user-filled.svg";
+import AccountOutline from "../../resources/icons/user.svg";
+
+// Navigation items
+const navItems = [
+  {
+    path: "/",
+    label: "Home",
+    iconFilled: HomeFilled,
+    iconOutline: HomeOutline,
+  },
+  {
+    path: "/track",
+    label: "Track",
+    iconFilled: TrackFilled,
+    iconOutline: TrackOutline,
+  },
+  {
+    path: "/account",
+    label: "Account",
+    iconFilled: AccountFilled,
+    iconOutline: AccountOutline,
+  },
+];
 
 export default function NavigationBar() {
-  const [homeActive, setHomeActive] = useState(false);
-  const [trackActive, setTrackActive] = useState(true);
-  const [accountActive, setAccountActive] = useState(false);
+  const location = useLocation();
 
   return (
-    <Navbar
-      bg="light"
-      data-bs-theme="light"
-      fixed="bottom"
-      className="h-20 bg-[#f3f3f3] py-0"
-    >
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar fixed="bottom" className="h-20 bg-[#f3f3f3] py-0">
       <Container>
-        <Nav className="nav-items pt-1">
-          <Nav.Link
-            as={Link}
-            to="/"
-            onClick={() => {
-              setHomeActive(true);
-              setAccountActive(false);
-              setTrackActive(false);
-            }}
-          >
-            <div className="flex flex-col items-center">
-              <HomeIcon isActive={homeActive} />
-              <p className="mb-0">Home</p>
-            </div>
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/track"
-            onClick={() => {
-              setHomeActive(false);
-              setAccountActive(false);
-              setTrackActive(true);
-            }}
-          >
-            <div className="flex flex-col items-center">
-              <TrackIcon isActive={trackActive} />
-              <p className="mb-0">Track</p>
-            </div>
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/account"
-            onClick={() => {
-              setHomeActive(false);
-              setAccountActive(true);
-              setTrackActive(false);
-            }}
-          >
-            <div className="flex flex-col items-center">
-              <AccountIcon isActive={accountActive} />
-              <p className="mb-0">Account</p>
-            </div>
-          </Nav.Link>
+        <Nav className="nav-items pt-2">
+          {navItems.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <Nav.Link
+                key={item.path}
+                as={Link}
+                to={item.path}
+                className={isActive ? "active" : ""}
+              >
+                <img
+                  src={isActive ? item.iconFilled : item.iconOutline}
+                  alt={item.label}
+                />
+                <span>{item.label}</span>
+              </Nav.Link>
+            );
+          })}
         </Nav>
       </Container>
     </Navbar>
