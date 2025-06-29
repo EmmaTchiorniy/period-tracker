69e1c054ebcc9a2af677b386da9d018a23da1771
Emma Tchiorniy <emmtcho@gmail.com>
2025-06-29 15:28:01 +0200

    build works now

--- app/shared/navigation-bar.tsx
+++ app/shared/navigation-bar.tsx
@@ -1,6 +1,6 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/cjs/Container.js";
+import Nav from "react-bootstrap/cjs/Nav.js";
+import Navbar from "react-bootstrap/cjs/Navbar.js";
 import "./navigation-bar.scss";
 import { Link } from "react-router";
 import { useState } from "react";
