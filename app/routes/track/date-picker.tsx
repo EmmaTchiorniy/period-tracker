ea6db0e4ae2f9aa89fba8ccfb4b3a820cd3f9c56
Emma Tchiorniy <emmtcho@gmail.com>
2025-06-30 17:26:47 +0200

    date picker fixed

--- app/routes/track/date-picker.tsx
+++ app/routes/track/date-picker.tsx
@@ -1,72 +1,37 @@
-import * as React from "react";
 import dayjs from "dayjs";
-import { Stack } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+import { DateInput } from "@mantine/dates";
+import "./date-picker.css";
 
 interface DatePickerProps {
   setDate: any;
 }
 
 export default function StyledDatePicker(props: DatePickerProps) {
-  const [value, setValue] = React.useState(dayjs());
+  const [value, setValue] = useState<string | null>(
+    dayjs().format("YYYY-MM-DD").toString()
+  );
+
+  const onChange = (newValue: string | null) => {
+    setValue(newValue);
+    props.setDate(dayjs(newValue?.toString()));
+  };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Stack spacing={2}>
-        <DatePicker
-          value={value}
-          onAccept={(value) => {
-            props.setDate(value);
-          }}
-          onChange={(newValue) => {
-            if (newValue != null) {
-              setValue(newValue);
-            }
-          }}
-          format="MMM D, YYYY"
-          slotProps={{
-            textField: {
-              variant: "standard",
-              InputProps: {
-                disableUnderline: true,
-              },
-              sx: {
-                backgroundColor: "#f5f5f5",
-                borderRadius: "10px",
-                width: "180px",
-                height: "32px",
-                padding: "0 12px",
-                "& .MuiInputBase-root": {
-                  backgroundColor: "#f5f5f5",
-                  borderRadius: "10px",
-                  height: "40px",
-                  display: "flex",
-                  alignItems: "center",
-                  "&:before": {
-                    display: "none",
-                  },
-                  "&:after": {
-                    display: "none",
-                  },
-                },
-                "& .MuiInputBase-input": {
-                  fontFamily: "mono",
-                  fontSize: "14px",
-                  color: "#333",
-                  textAlign: "left",
-                  lineHeight: "40px",
-                },
-                "& .MuiInputAdornment-root": {
-                  marginLeft: "8px",
-                  marginBottom: "4px",
-                },
-              },
-            },
-          }}
-        />
-      </Stack>
-    </LocalizationProvider>
+    <DateInput
+      value={value}
+      maxDate={dayjs().format("YYYY-MM-DD").toString()}
+      onChange={onChange}
+      label=" "
+      placeholder={dayjs().format("MMM DD, YYYY").toString()}
+      popoverProps={{ zIndex: 2000 }}
+      classNames={{
+        levelsGroup: "levels",
+        calendarHeader: "header",
+        weekday: "weekday",
+        day: "day",
+        calendarHeaderLevel: "header-level",
+      }}
+    />
   );
 }
